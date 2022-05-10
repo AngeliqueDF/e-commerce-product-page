401216be3a32ef8c4f07be35f015d323104cfc30
angelique-df <faye.angelique.d@gmail.com>
2022-05-10 20:53:12 +0000

    Add event listeners for carousels and lightbox

--- src/js/app.js
+++ src/js/app.js
@@ -19,13 +19,47 @@ window.addEventListener("DOMContentLoaded", () => {
 	toggleHide(".menu-toggle", ".header-menu");
 	toggleHide("#shopping-cart-toggle", "#shopping-cart");
 
-	const carousel = new Carousel(
-		LARGE_IMAGE_SELECTOR,
-		THUMBNAILS_ROW_SELECTOR,
-		PREVIOUS_BUTTON_SELECTOR,
-		NEXT_BUTTON_SELECTOR,
-		LARGE_IMAGES_PATHS
-	);
+	if (window.innerWidth <= 375) {
+		const mobileCarousel = new MobileCarousel(
+			LARGE_IMAGE_SELECTOR,
+			PREVIOUS_BUTTON_SELECTOR,
+			NEXT_BUTTON_SELECTOR,
+			LARGE_IMAGES_PATHS
+		);
+	} else {
+		const desktopCarousel = new DesktopCarousel(
+			LARGE_IMAGE_SELECTOR,
+			THUMBNAILS_ROW_SELECTOR,
+			PREVIOUS_BUTTON_SELECTOR,
+			NEXT_BUTTON_SELECTOR,
+			LARGE_IMAGES_PATHS
+		);
 
-	const largeImage = document.querySelector(LARGE_IMAGE_SELECTOR);
+		document
+			.querySelector(LARGE_IMAGE_SELECTOR)
+			.addEventListener("click", () => {
+				desktopCarousel.renderLightBox();
+				desktopCarousel.renderLightboxOverlay();
+
+				const lightboxElement = document.querySelector(".lightbox");
+				const lightboxOverlay = document.querySelector(".lightbox-overlay");
+
+				// TODO either create a Lightbox class or move this functionality to DesktopCarousel
+				// When the overlay is clicked, we remove it as well as the the lightbox
+				lightboxOverlay.addEventListener("click", () => {
+					lightboxElement.remove();
+					lightboxOverlay.remove();
+				});
+
+				const lightbox = new DesktopCarousel(
+					".lightbox " + LARGE_IMAGE_SELECTOR,
+					".lightbox " + THUMBNAILS_ROW_SELECTOR,
+					".lightbox " + PREVIOUS_BUTTON_SELECTOR,
+					".lightbox " + NEXT_BUTTON_SELECTOR,
+					LARGE_IMAGES_PATHS
+				);
+
+				lightbox.initializeButtons();
+			});
+	}
 });
