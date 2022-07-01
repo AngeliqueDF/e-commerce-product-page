99b5be6d079a631c13a33dc91018bcfec36bb2f1
angelique-df <faye.angelique.d@gmail.com>
2022-07-01 12:37:24 +0000

    Update MobileCarousel.js

--- src/js/MobileCarousel.js
+++ src/js/MobileCarousel.js
@@ -5,8 +5,6 @@ class MobileCarousel {
 		nextImageButtonSelector,
 		largeImagesPaths
 	) {
-		console.log(this);
-
 		this.largeImageElement = document.querySelector(largeImageSelector);
 
 		this.previousImageButton = document.querySelector(
@@ -68,8 +66,6 @@ class MobileCarousel {
 		const indexOfImage = this.largeImagesPaths.findIndex(
 			(image) => image === this.currentLargeImgPath()
 		);
-
-		console.trace(this.currentLargeImgPath());
 
 		// If the last image is currently displayed, we replace it by the first one.
 		if (indexOfImage === this.largeImagesPaths.length - 1) {
