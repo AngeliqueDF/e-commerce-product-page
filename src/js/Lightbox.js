9ff783a49132d1db9c909c6c25d43d06883d11bc
angelique-df <faye.angelique.d@gmail.com>
2022-07-01 13:36:49 +0000

    Update Lightbox.js
    Add back the logic to render the lightbox and its overlay
    
    Removed the methods `this.renderOverlay()` and `this.renderLightbox(thumbnails)`responsible for these tasks because:
    - `this` can't be called before `super()` in the constructor.
    - `super()` won't work if it's called before the lightbox and its overlay aren't present in the DOM.
    
    Hence the lightbox and its overlay are added to the DOM by the constructor, then the `super` method is called.

--- src/js/Lightbox.js
+++ src/js/Lightbox.js
@@ -12,12 +12,48 @@ class Lightbox extends DesktopCarousel {
 		lightboxCloseButtonSelector,
 		THUMBNAIL_IMAGES_PATHS
 	) {
+		// Render the overlay
+		const lightboxOverlay = document.createElement("div");
+		lightboxOverlay.classList.add("lightbox-overlay");
+		document.body.querySelector("main").appendChild(lightboxOverlay);
+
+		const LIGHTBOX_TEMPLATE = (thumbnails) => `
+				<button class="close-lightbox" title="Close lightbox"></button>
+				<button class="product-image-controller previous-image"></button>
+				<button class="product-image-controller next-image"></button>
+
+				<img
+					class="large-product-image"
+					src=${largeImageOne}
+					alt=""
+				/>
+
+				<div class="image-thumbnails">
+				${thumbnails
+					.map(
+						(thumbnail) =>
+							'<div class="thumbnail-container"><img src="' +
+							thumbnail +
+							'" alt="" /></div>'
+					)
+					.join("")}
+				</div>
+    	`;
+		// Render the lightbox
+		const lightboxElement = document.createElement("div");
+		lightboxElement.innerHTML = LIGHTBOX_TEMPLATE(THUMBNAIL_IMAGES_PATHS);
+		lightboxElement.classList.add("lightbox");
+		document.querySelector("main").appendChild(lightboxElement);
+
 		super(largeImageElement, thumbnailsRowSelector, largeImagesPaths);
 
 		this.previousImageButtonSelector = previousImageButtonSelector;
 		this.nextImageButtonSelector = nextImageButtonSelector;
 		this.lightboxOverlaySelector = lightboxOverlaySelector;
 		this.lightboxCloseButtonSelector = lightboxCloseButtonSelector;
+
+		this.lightboxOverlay = lightboxOverlay;
+		this.lightboxElement = lightboxElement;
 
 	}
 }
