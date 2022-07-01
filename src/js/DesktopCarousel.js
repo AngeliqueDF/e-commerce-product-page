977094ee8ce380717dd839232d49ed00c936eb89
angelique-df <faye.angelique.d@gmail.com>
2022-07-01 13:39:28 +0000

    Update DesktopCarousel.js
    Remove logic to control lightbox' next/previous buttons.

--- src/js/DesktopCarousel.js
+++ src/js/DesktopCarousel.js
@@ -9,11 +9,6 @@ class DesktopCarousel {
 			document.querySelectorAll(thumbnailsRowSelector)
 		);
 
-		this.previousImageButton = document.querySelector(
-			previousImageButtonSelector
-		);
-		this.nextImageButton = document.querySelector(nextImageButtonSelector);
-
 		// On desktop, thumbnails clicking a thumbnail updates the large image
 		this.thumbnailElements.forEach((thumbnail, index) => {
 			thumbnail.addEventListener("click", (e) => {
@@ -22,16 +17,6 @@ class DesktopCarousel {
 		});
 
 		this.largeImagesPaths = largeImagesPaths;
-	}
-
-	initializeButtons() {
-		this.previousImageButton.addEventListener("click", () => {
-			this.displayPreviousImage();
-		});
-
-		this.nextImageButton.addEventListener("click", () => {
-			this.displayNextImage();
-		});
 	}
 
 	updateSelectedThumbnail(element, index) {
@@ -52,41 +37,6 @@ class DesktopCarousel {
 	replaceLargeImagePath = (newPath) => {
 		this.largeImageElement.src = newPath;
 	};
-
-	currentLargeImgPath = function () {
-		return new URL(this.largeImageElement.src).pathname;
-	};
-
-	displayPreviousImage() {
-		// Find the index of the current path in currentLargeImgPath. To determine if we need to move to the last image.
-		const indexOfImage = this.largeImagesPaths.findIndex(
-			(image) => image === this.currentLargeImgPath()
-		);
-
-		// If the first image in the list is displayed, we replace it with the last image.
-		if (indexOfImage === 0) {
-			this.replaceLargeImagePath(
-				`${this.largeImagesPaths[this.largeImagesPaths.length - 1]}`
-			);
-		} else {
-			const newIndex = indexOfImage - 1;
-			this.replaceLargeImagePath(`${this.largeImagesPaths[newIndex]}`);
-		}
-	}
-
-	displayNextImage() {
-		const indexOfImage = this.largeImagesPaths.findIndex(
-			(image) => image === this.currentLargeImgPath()
-		);
-
-		// If the last image is currently displayed, we replace it by the first one.
-		if (indexOfImage === this.largeImagesPaths.length - 1) {
-			this.replaceLargeImagePath(`${this.largeImagesPaths[0]}`);
-		} else {
-			const newIndex = indexOfImage + 1;
-			this.replaceLargeImagePath(`${this.largeImagesPaths[newIndex]}`);
-		}
-	}
 }
 
 export default DesktopCarousel;
